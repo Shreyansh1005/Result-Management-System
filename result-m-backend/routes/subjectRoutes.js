803905018ba815fb16dfe9579909216254b558f6
Shivam Dubey <shivam.2000shivu@gmail.com>
2024-08-26 18:45:37 +0530

    Remove all files from Git's tracking

--- result-m-backend/routes/subjectRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createSubject, getAllSubjects } = require('../controllers/subjectController');
-const { protect, admin } = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.route('/').post(protect, admin, createSubject).get(protect);
-router.get('/',protect, getAllSubjects);
-
-module.exports = router;
